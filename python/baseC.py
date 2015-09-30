0032a1dcdfe146d29e0444907f4fedfc0cca4820
kalq <bassam.saeed@gmail.com>
2015-09-30 14:22:23 -0400

    Probably last version. Might comment it more later. Might create another function for recurring code. Might not.

--- python/baseC.py
+++ python/baseC.py
@@ -1,41 +1,77 @@
 import sys
 
 def usage():
-    print ("Usage: baseC.py [BASE SYSTEM] [INTEGER]\n")
+    print ("Usage: baseC.py [INPUT BASE SYSTEM]::[OUTPUT BASE SYSTEM] [INTEGER]\n")
+    print ("[BASE SYSTEM] as either numerical digits or their corresponding names as defined below\n")
     print ("Base Systems:")
     print ("\tBase 2: binary")
-    print ("\tBase 16: hex")
-    
-def toHex(x):
-    mod = x % 16
-    digits = "0123456789abcdef"
+    print ("\tBase 10: decimal")
+    print ("\tBase 16: hex\n")
+    print ("Example:")
+    print ("baseC.py hex::decimal 4e")
+    print ("baseC.py 10::24 1337")
 
-    if (x > 15):
-        return toHex(int(x/16)) + digits[mod]
+def toBase(inp, base):
+    mod = inp % base
+    digits = "0123456789abcdefghijklmnopqrstuvwxyz"
+
+    if (inp >= base):
+        return toBase(int(inp/base), base) + digits[mod]
     else:
         return digits[mod]
 
-def toBinary(x):
-    mod = x % 2
-    digits = "01"
-    
-    if (x > 1):
-        return toBinary(int(x/2)) + digits[mod]
-    else:
-        return digits[mod]
+def toDecimal(inp, base):
+    alp, dig = list(range(10, 36)), "abcdefghijklmnopqrstuvwxyz"
+ 
+    length = len(str(inp)) - 1
+    res = 0
+    for i in str(inp):
+        try:                                            # Checks to see if the digit is numerical
+            res += int(i) * base ** length              # No changes needed
+        except ValueError:
+            res += alp[dig.index(i)] * base ** length   # If not, replaces alphabet character with corresponding numerical digit
+        length -= 1
 
+    return res
+ 
 orig = sys.argv[1:]
 
 try:
+    init,to = orig[0].split("::")
     inp = int(orig[1])
 except:
     usage()
     sys.exit(2)
 
-if (orig[0] == "binary" or orig[0] == "-b"):
-    print(toBinary(inp))
-elif (orig[0] == "hex" or orig[0] == "-hx"):
-    print(toHex(inp))
-else:
-    usage()
-    sys.exit(2)
+try:
+    init = int(init)
+except ValueError:
+    if (init == "binary"):
+        init = 2
+    elif (init == "decimal"):
+        init = 10
+    elif (init == "hex"):
+        init = 16
+    else:
+        usage()
+        sys.exit(2)
+
+try:
+    to = int(to)
+except ValueError:
+    if (to == "binary"):
+        to = 2
+    elif (to == "decimal"):
+        to = 10
+    elif (to == "hex"):
+        to = 16
+    else:
+        usage()
+        sys.exit(2)
+
+if (init != 10):
+    inp = toDecimal(inp, init)
+
+inp = toBase(inp, to)
+
+print (inp)
